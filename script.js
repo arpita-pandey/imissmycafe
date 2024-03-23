5b33790a54d976a3993e9f0492f362c6fe986219
cemarsh4 <caitlin.e.marsh@icloud.com>
2024-03-22 23:12:11 -0700

    Update 3/22

--- script.js
+++ script.js
@@ -4,10 +4,22 @@
 //switch image1.jpg and image2.jpg with paths to actual images
 document.getElementById("changeButton").addEventListener("click", function() {
     var currentImage = document.getElementById("mainImage").src;
-    if (currentImage.endsWith("IMG_6776.jpeg")) {
-        document.getElementById("mainImage").src = "Cat.jpeg";
+    if (currentImage.endsWith("Colored_Merged.jpg")) {
+        document.getElementById("mainImage").src = "Cat_1.jpg";
     }
-    else if (currentImage.endsWith("Cat.jpeg")) {
-        document.getElementById("mainImage").src = "IMG_6776.jpeg";
+    else if (currentImage.endsWith("Cat_1.jpg")) {
+        document.getElementById("mainImage").src = "Cat_2.jpg";
+    }
+    else if (currentImage.endsWith("Cat_2.jpg")) {
+        document.getElementById("mainImage").src = "Fish.jpg";
+    }
+    else if (currentImage.endsWith("Fish.jpg")) {
+        document.getElementById("mainImage").src = "Plants.jpg";
+    }
+    else if (currentImage.endsWith("Plants.jpg")) {
+        document.getElementById("mainImage").src = "Cups.jpg";
+    }
+    else if (currentImage.endsWith("Cups.jpg")) {
+        document.getElementById("mainImage").src = "Colored_Merged.jpg";
     }
 });
