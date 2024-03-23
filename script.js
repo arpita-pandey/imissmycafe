03975d0fdb7b2dc86a9e51c67df1c7e2b26330ef
cemarsh4 <caitlin.e.marsh@icloud.com>
2024-03-23 11:55:50 -0700

    update

--- script.js
+++ script.js
@@ -4,22 +4,22 @@
 //switch image1.jpg and image2.jpg with paths to actual images
 document.getElementById("changeButton").addEventListener("click", function() {
     var currentImage = document.getElementById("mainImage").src;
-    if (currentImage.endsWith("Plants2.png")) {
-        document.getElementById("mainImage").src = "Cat_1.png";
+    if (currentImage.endsWith("CM.png")) {
+        document.getElementById("mainImage").src = "Cat12.png";
     }
-    else if (currentImage.endsWith("Cat_1.png")) {
-        document.getElementById("mainImage").src = "Cat_2.png";
+    else if (currentImage.endsWith("Cat12.png")) {
+        document.getElementById("mainImage").src = "Cat22.png";
     }
-    else if (currentImage.endsWith("Cat_2.png")) {
+    else if (currentImage.endsWith("Cat22.png")) {
         document.getElementById("mainImage").src = "Fish2.png";
     }
     else if (currentImage.endsWith("Fish2.png")) {
-        document.getElementById("mainImage").src = "Plants.png";
-    }
-    else if (currentImage.endsWith("Plants.png")) {
-        document.getElementById("mainImage").src = "Cups.png";
-    }
-    else if (currentImage.endsWith("Cups.png")) {
         document.getElementById("mainImage").src = "Plants2.png";
+    }
+    else if (currentImage.endsWith("Plants2.png")) {
+        document.getElementById("mainImage").src = "Cups2.png";
+    }
+    else if (currentImage.endsWith("Cups2.png")) {
+        document.getElementById("mainImage").src = "CM.png";
     }
 });
